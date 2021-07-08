095db1b51cb172644a6c8ae316a7ce06c4a8675f
Javad <javad.zahedi2001@gmail.com>
2021-07-08 11:27:09 +0430

    efficient solution was added

--- problem1.py
+++ problem1.py
@@ -1,8 +1,19 @@
-def main():
+def old_main(): # Non efficient solution
     seq = filter(lambda x: (x%3 == 0 or x%5 == 0), range(1, 1000))
     result = sum(seq)
     print(result)
 
 
+def multipliers_sum(n, k):
+    seq_length = (n-1) // k
+    return k * seq_length * (seq_length+1) / 2
+
+
+def new_main(): # efficient solution
+    result = (multipliers_sum(1000, 3) + multipliers_sum(1000, 5)
+             - multipliers_sum(1000, 15))
+    print(int(result))
+
+
 if __name__ == '__main__':
-    main()
+    new_main()
