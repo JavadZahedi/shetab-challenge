06194890caa397f2343e7d5e7fdd52df12b06cc2
Javad <javad.zahedi2001@gmail.com>
2021-07-08 10:56:43 +0430

    first problem was edited

--- first.py
+++ first.py
@@ -1,9 +1,7 @@
 def main():
-    sum = 0
-    for i in range(1, 1000):
-        if i % 3 == 0 or i % 5 == 0:
-            sum += i
-    print(sum)
+    seq = filter(lambda x: (x%3 == 0 or x%5 == 0), range(1, 1000))
+    result = sum(seq)
+    print(result)
 
 
 if __name__ == '__main__':
